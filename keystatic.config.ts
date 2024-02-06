4f0c07dfe90a5474e894766fc4b3db16f68a90b5
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2024-02-05 22:08:39 -0600

    ....

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: {
+      owner: 'ChristianSantanaLlanes',
+      name: 'astro-web-game-videogame'
+    }
   },
   ui: {
     brand: {
